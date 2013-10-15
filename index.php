ab2e256cfca9b1e126cde23ea135b8c75d64ef27
jpz <jpz@192.168.192.5>
2013-10-15 21:52:36 +0200

    I'm I doing it?

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+      echo" The same thing we do every night Pinky"
+        ?>
+    </body>
+</html>
